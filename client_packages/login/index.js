84435cd1a47ef86819f30975c45f4f3440b075ea
gigatev19 <74433499+gigatev19@users.noreply.github.com>
2025-04-20 10:33:32 +0200

    Update index.js

--- client_packages/login/index.js
+++ client_packages/login/index.js
@@ -5,8 +5,8 @@ mp.events.add('playerReady', () => {
     mp.gui.chat.push("✅ login/index.js wurde geladen");
   mp.events.call('showLogin');
   mp.gui.cursor.show(true, true); // Cursor anzeigen
-  mp.gui.chat.activate(false); // Chat deaktivieren
-  mp.game.ui.displayRadar(false); // Minimap ausblenden
-  mp.game.ui.displayHud(false); // HUD deaktivieren
+  //mp.gui.chat.activate(false); // Chat deaktivieren
+  //mp.game.ui.displayRadar(false); // Minimap ausblenden
+  //mp.game.ui.displayHud(false); // HUD deaktivieren
   mp.players.local.freezePosition = true; // Spieler einfrieren
 });
