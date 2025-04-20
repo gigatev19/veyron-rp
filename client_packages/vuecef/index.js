298576d3656fbd2c1a6ec931d5c6494d8853f882
gigatev19 <74433499+gigatev19@users.noreply.github.com>
2025-04-20 15:15:03 +0200

    Update index.js

--- client_packages/vuecef/index.js
+++ client_packages/vuecef/index.js
@@ -2,7 +2,9 @@ mp.events.add('showLogin', () => {
     mp.gui.chat.push("✅ showLogin called");
     const browser = mp.browsers.new('package://vuecef-build/index.html');
 
-    mp.gui.cursor.show(true, true);
+    setTimeout(() => {
+        mp.gui.cursor.show(true, true);
+      }, 50);
     mp.gui.chat.push("✅ CEF geöffnet & Cursor sollte sichtbar sein");
     mp.gui.chat.activate(false);
     mp.game.ui.displayRadar(false);
