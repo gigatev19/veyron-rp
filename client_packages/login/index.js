64e4423697b8a8287c9e5fa46b577df9988fae8e
gigatev19 <74433499+gigatev19@users.noreply.github.com>
2025-04-20 15:02:50 +0200

    update login

--- client_packages/login/index.js
+++ client_packages/login/index.js
@@ -2,13 +2,8 @@
 let loginBrowser = null;
 
 mp.events.add('playerReady', () => {
-    mp.gui.chat.push("✅ login/index.js wurde geladen");
+  mp.gui.chat.push("✅ login/index.js wurde geladen");
   mp.events.call('showLogin');
-  mp.gui.cursor.show(true, true); // Cursor anzeigen
-  mp.gui.chat.activate(false); // Chat deaktivieren
-  mp.game.ui.displayRadar(false); // Minimap ausblenden
-  mp.game.ui.displayHud(false); // HUD deaktivieren
-  mp.players.local.freezePosition = true; // Spieler einfrieren
 });
 
 // client_packages/login/index.js
--- client_packages/vuecef/index.js
+++ client_packages/vuecef/index.js
@@ -1,5 +1,11 @@
 mp.events.add('showLogin', () => {
     mp.gui.chat.push("✅ showLogin called");
     const browser = mp.browsers.new('package://vuecef-build/index.html');
+
+    mp.gui.cursor.show(true, true);
+    mp.gui.chat.activate(false);
+    mp.game.ui.displayRadar(false);
+    mp.game.ui.displayHud(false);
+    mp.players.local.freezePosition = true;
   });
   
