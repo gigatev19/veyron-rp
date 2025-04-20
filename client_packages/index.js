3a95c881eb9144a1033cf180060cfcacca274417
gigatev19 <74433499+gigatev19@users.noreply.github.com>
2025-04-20 10:35:08 +0200

    Update index.js

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,8 +1,8 @@
 require('./login/index.js');
 
 mp.gui.cursor.show(true, true);
-mp.gui.chat.activate(false);
-mp.gui.chat.show(false);
+//mp.gui.chat.activate(false);
+//mp.gui.chat.show(false);
 
 // Auth CEF anzeigen
 mp.events.add('client:auth:jwt', (token) => {
